8dcb7a4cc52c7c87c5d6401af54fdc3ccc4ba882
jf10 <unicom_jf10@sina.com>
2025-01-03 11:34:27 +0800

    fix package problem

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,16 @@
 package main
 
 import "fmt"
+import pb "github.com/unicom-jf/learning-protobuf/backend/learningpb"
 
 func main() {
-
+	pb.Person{
+    Id:    1234,
+    Name:  "John Doe",
+    Email: "jdoe@example.com",
+    Phones: []*pb.Person_PhoneNumber{
+        {Number: "555-4321", Type: pb.PhoneType_PHONE_TYPE_HOME},
+    },
+	}
 	fmt.Println("ok")
 }
